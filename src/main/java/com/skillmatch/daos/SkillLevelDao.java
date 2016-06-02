ec9523f5c6211510bf6adcd0461129812724b79b
Alejandro Germenos <agermenos@agermenos-mbp.local>
2016-06-02 16:42:51 -0700

    Added classes

---
+++ src/main/java/com/skillmatch/daos/SkillLevelDao.java
@@ -0,0 +1,11 @@
+package com.skillmatch.daos;
+
+import com.skillmatch.pojos.SkillLevel;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by agermenos on 6/2/16.
+ */
+@Repository("skillLevelDao")
+public class SkillLevelDao extends GeneralDao<SkillLevel> {
+}
---
+++ src/main/java/com/skillmatch/daos/UserSkillDao.java
@@ -0,0 +1,11 @@
+package com.skillmatch.daos;
+
+import com.skillmatch.pojos.UserSkill;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by agermenos on 6/2/16.
+ */
+@Repository("userSkillDao")
+public class UserSkillDao extends GeneralDao<UserSkill>{
+}
