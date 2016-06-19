3d948c215fabe363bff4c25e424be598371f4fc4
Alejandro <agermenos@gmail.com>
2016-06-18 19:57:24 -0700

    Updated Reverse Polish Notation with push method.

--- src/main/java/com/skillmatch/tests/ReversePolishNotation.java
+++ src/main/java/com/skillmatch/tests/ReversePolishNotation.java
@@ -1,10 +1,16 @@
 package com.skillmatch.tests;
 
+import org.springframework.expression.spel.ast.Operator;
+
+import java.util.Stack;
+
 /**
  * Created by agermenos on 6/17/16.
  */
 
 public class ReversePolishNotation {
+    Stack<String> mainStack =new Stack<>();
+
     public enum Operators {
         ADD, SUBTRACT, DIVIDE, MULTIPLY, EXPONENT, REMAINDER, ERROR;
         public Operators getSymbol(String operator) {
@@ -22,6 +28,19 @@ public class ReversePolishNotation {
         }
     };
 
+    public String calculate (Double value1, Double value2, Operators selectedOperation) {
+        switch (selectedOperation) {
+            case ADD: return new Double (value1 + value2).toString();
+            case SUBTRACT: return new Double (value1 - value2).toString();
+            case MULTIPLY: return new Double (value1 * value2).toString();
+            case DIVIDE: return new Double (value1 / value2).toString();
+            case EXPONENT: return (Math.pow(value1, value2)+"");
+            case REMAINDER: return new Double (value1 % value2).toString();
+            case ERROR: return "Not an acceptable operation: " + selectedOperation;
+            default: return null;
+        }
+    }
+
     public String calculate(String input){
         System.out.print (input + " = ");
         String[] strValues = input.split(" ");
@@ -29,16 +48,23 @@ public class ReversePolishNotation {
         Double value2 = Double.parseDouble(strValues[1]);
         String operator = strValues[2];
         Operators selectedOperation = Operators.ERROR.getSymbol(operator);
-        switch (selectedOperation) {
-            case ADD: return new Double (value1 + value2).toString();
-            case SUBTRACT: return new Double (value1 - value2).toString();
-            case MULTIPLY: return new Double (value1 * value2).toString();
-            case DIVIDE: return new Double (value1 / value2).toString();
-            case EXPONENT: return (Math.pow(Integer.parseInt(strValues[0]),(Integer.parseInt(strValues[1])))+"");
-            case REMAINDER: return new Double (value1 % value2).toString();
-            case ERROR: return "Not an acceptable operation: " + operator;
+        return calculate(value1, value2, selectedOperation);
+    }
+
+    public void push(String value){
+        if (!Operators.ERROR.getSymbol(value).equals(Operators.ERROR)) {
+            Double value2 = Double.parseDouble(mainStack.pop());
+            Double value1 = Double.parseDouble(mainStack.pop());
+            mainStack.push(calculate(
+                    value1,
+                    value2,
+                    Operators.ERROR.getSymbol(value)
+            ));
         }
-        return "Unreachable end";
+        else {
+            mainStack.push(value);
+        }
+        System.out.println(mainStack.toString());
     }
 
     public static void main(String args[]) {
@@ -49,6 +75,15 @@ public class ReversePolishNotation {
         System.out.println (rpn.calculate("5 2 ^"));
         System.out.println (rpn.calculate("234 50 %"));
         System.out.println (rpn.calculate("1 2 T "));
+        rpn.push("5");
+        rpn.push("4");
+        rpn.push("*");
+        rpn.push("2");
+        rpn.push("/");
+        rpn.push("3");
+        rpn.push("%");
+        rpn.push("10");
+        rpn.push("^");
     }
 }
 
